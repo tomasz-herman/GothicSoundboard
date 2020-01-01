eea86d17c026e855d98f7dd8c19c707c2e865297
tomasz-herman <tomasz.herman@protonmail.com>
2020-01-01 08:24:12 +0100

    Added favorite dialog feature

--- app/src/main/java/com/therman/gothicsoundboard/fragments/DialogAdapter.java
+++ app/src/main/java/com/therman/gothicsoundboard/fragments/DialogAdapter.java
@@ -1,9 +1,12 @@
 package com.therman.gothicsoundboard.fragments;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,18 +30,24 @@ public class DialogAdapter extends RecyclerView.Adapter<DialogAdapter.ViewHolder
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         TextView tvDialogFrom, tvDialogText;
+        ImageView ivFavorite;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tvDialogFrom = itemView.findViewById(R.id.tvDialogFrom);
             tvDialogText = itemView.findViewById(R.id.tvDialogText);
+            ivFavorite = itemView.findViewById(R.id.ivFavorite);
             itemView.setOnClickListener(v -> Toast.makeText(context, "Playing: " + ((Dialog)v.getTag()).getFile(), Toast.LENGTH_SHORT).show());
+            ivFavorite.setOnClickListener(v -> {
+                SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+                if(prefs.contains((String)v.getTag())){
+                    ivFavorite.setImageResource(R.drawable.unfavorite);
+                    prefs.edit().remove((String)v.getTag()).apply();
+                } else {
+                    ivFavorite.setImageResource(R.drawable.favorite);
+                    prefs.edit().putBoolean((String)v.getTag(), true).apply();
+                }
+            });
         }
-    }
-
-    public void replace(ArrayList<Dialog> dialogs){
-        this.dialogs.clear();
-        this.dialogs = dialogs;
-        notifyDataSetChanged();
     }
 
     @NonNull
@@ -51,6 +60,11 @@ public class DialogAdapter extends RecyclerView.Adapter<DialogAdapter.ViewHolder
     @Override
     public void onBindViewHolder(@NonNull DialogAdapter.ViewHolder holder, int position) {
         holder.itemView.setTag(dialogs.get(position));
+        holder.ivFavorite.setTag(dialogs.get(position).getFile());
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        if(prefs.contains(dialogs.get(position).getFile()))
+            holder.ivFavorite.setImageResource(R.drawable.favorite);
+        else holder.ivFavorite.setImageResource(R.drawable.unfavorite);
         holder.tvDialogFrom.setText(dialogs.get(position).getFrom().getName());
         holder.tvDialogText.setText(dialogs.get(position).getText());
     }
